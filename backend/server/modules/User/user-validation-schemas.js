39dd128b357aae1660acaacfbb48407c6265c7c1
SahilAkbari <sahilakbari1111@gmail.com>
2025-10-17 16:24:05 +0530

    schema fix

--- backend/server/modules/User/user-validation-schemas.js
+++ backend/server/modules/User/user-validation-schemas.js
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 const registerSchema = Joi.object({
   name: Joi.string().trim().allow("").max(100).optional(),
-  email: Joi.string().trim().email().required(),
+  email: Joi.string().lowercase().trim().email().required(),
   password: Joi.string().trim().min(6).max(128).required(),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().trim().email().required(),
-  password: Joi.string().trim().lowercase().min(6).max(128).required(),
+  email: Joi.string().lowercase().trim().email().required(),
+  password: Joi.string().trim().min(6).max(128).required(),
 });
 
 export { registerSchema, loginSchema };
